bd0f3104d02dda7f7793e8fe14b9140d71089278
cornelas24-pixel <cornelas_24@alu.uabcs.mx>
2026-02-26 13:26:14 -0600

    unidad 2 ejercicio 17

--- src/Main/Ventana.java
+++ src/Main/Ventana.java
@@ -1,259 +1,150 @@
 package Main;
 import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+
 public class Ventana extends JFrame
 {
 	public Ventana() 
 	{
-		
-		this.setVisible(true);
 		this.setSize(1000,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setMinimumSize(new Dimension(200,200));
-		this.setMaximumSize(new Dimension(800,800));
-		this.setTitle("login");
+		this.setMinimumSize(new Dimension(400,300));
+		this.setTitle("Usuarios");
 		this.setLocationRelativeTo(null);
 		this.setLayout(null);
-		
-		//contenedor gris (fondo)
+
+		// fondo
 		JPanel contenedor = new JPanel();
 		contenedor.setOpaque(true);
 		contenedor.setBackground(new Color(240,240,240));
-		contenedor.setSize(1000,500);
+		contenedor.setSize(1000,600);
 		contenedor.setLayout(null);
 		contenedor.setLocation(0,0);
 		this.add(contenedor);
-		
-		// tarjeta dentro de contenedor
-		JPanel card = new JPanel();
-		card.setLayout(null);
-		card.setBackground(Color.white);
-		card.setSize(320,400);
-		card.setLocation(90,60);
-		card.setBorder(BorderFactory.createLineBorder(new Color(220,220,220),1));
-		contenedor.add(card);
-		
-		//png (logo centrado)
-		ImageIcon icon = new ImageIcon("src/Main/images.png");
-		JLabel logo = new JLabel(icon);
-		logo.setBounds(128,20,64,64);
-		card.add(logo);
 
-		
-		//titulo login
-		JLabel title_login = new JLabel();
-		title_login.setText("Bienvenido");
-		title_login.setSize(320,40);
-		title_login.setLocation(0,95);
-		title_login.setFont(new Font("Arial",Font.BOLD,24));
-		title_login.setHorizontalAlignment(JLabel.CENTER);
-		card.add(title_login);
-		
-		//ingresar nombre
-		JTextField username = new JTextField();
-		username.setSize(240,35);
-		username.setLocation(40,150);
-		username.setBackground(Color.white);
-		username.setFont(new Font("Arial",Font.PLAIN,16));
-		username.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200),1),BorderFactory.createEmptyBorder(5,10,5,10)));
-		card.add(username);
-		
-		//contraseña
-		JPasswordField password = new JPasswordField();
-		password.setSize(240,35);
-		password.setLocation(40,200);
-		password.setBackground(Color.white);
-		password.setFont(new Font("Arial",Font.PLAIN,16));
-		password.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200),1),BorderFactory.createEmptyBorder(5,10,5,10)));
-		card.add(password);
-		
-		//boton de acceder
-		JButton acceder = new JButton();
-		acceder.setSize(240,40);
-		acceder.setLocation(40,255);
-		acceder.setBackground(new Color(66,133,244));
-		acceder.setForeground(Color.white);
-		acceder.setFont(new Font("Arial",Font.BOLD,16));
-		acceder.setText("Acceder");
-		acceder.setBorder(BorderFactory.createLineBorder(new Color(66,133,244),1));
-		card.add(acceder);
-		
-		JCheckBox remember = new JCheckBox();
-		remember.setSize(240,20);
-		remember.setLocation(40,305);
-		remember.setBackground(Color.white);
-		remember.setFont(new Font("Arial",Font.PLAIN,13));
-		remember.setText("Recuérdame");
-		card.add(remember);
-		
-		//olvido contraseña
-		JLabel forgot_pass = new JLabel();
-		forgot_pass.setText("¿Olvidaste tu contraseña?");
-		forgot_pass.setSize(240,25);
-		forgot_pass.setLocation(40,325);
-		forgot_pass.setFont(new Font("Arial",Font.PLAIN,13));
-		forgot_pass.setForeground(new Color(66,133,244));
-		forgot_pass.setHorizontalAlignment(JLabel.CENTER);
-		card.add(forgot_pass);
-		
-		//boton de crear cuenta
-		JButton new_account = new JButton();
-		new_account.setSize(240,35);
-		new_account.setLocation(40,350);
-		new_account.setBackground(Color.white);
-		new_account.setFont(new Font("Arial",Font.BOLD,14));
-		new_account.setText("Crear cuenta");
-		new_account.setBorder(BorderFactory.createLineBorder(new Color(200,200,200),1));
-		card.add(new_account);
-		
-		//REGISTER --------------------------------------------------
-		
-		JPanel register_container = new JPanel();
-		register_container.setLayout(null);
-		register_container.setBackground(Color.white);
-		register_container.setSize(320,400);
-		register_container.setLocation(450,60);
-		register_container.setBorder(BorderFactory.createLineBorder(new Color(220,220,220),1));
-		contenedor.add(register_container);
-		
-		//titulo login
-		JLabel title_register = new JLabel();
-		title_register.setText("Registro");
-		title_register.setSize(320,40);
-		title_register.setLocation(0,35);
-		title_register.setFont(new Font("Arial",Font.BOLD,24));
-		title_register.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(title_register);
-		
-		JLabel nombre_usuario = new JLabel();
-		nombre_usuario.setText("Nombre de usuario");
-		nombre_usuario.setSize(320,105);
-		nombre_usuario.setLocation(0,35);
-		nombre_usuario.setFont(new Font("Arial",Font.BOLD,12));
-		nombre_usuario.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(nombre_usuario);
-		 
-		JTextField username_reg = new JTextField();
-		username_reg.setSize(240,35);
-		username_reg.setLocation(40,100);
-		username_reg.setBackground(Color.white);
-		username_reg.setFont(new Font("Arial",Font.PLAIN,16));
-		username_reg.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200),1),BorderFactory.createEmptyBorder(5,10,5,10)));
-		register_container.add(username_reg);
-		
-		JLabel bio_text = new JLabel();
-		bio_text.setText("Biografia");
-		bio_text.setSize(320,105);
-		bio_text.setLocation(0,100);
-		bio_text.setFont(new Font("Arial",Font.BOLD,12));
-		bio_text.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(bio_text);
-		
-		JTextArea bio = new JTextArea();
-		bio.setBounds(40,160, 240, 60);
-		bio.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200),1),BorderFactory.createEmptyBorder(5,10,5,10)));
-		register_container.add(bio);
-		
-		JLabel preference_text = new JLabel();
-		preference_text.setText("Preferencias");
-		preference_text.setSize(320,105);
-		preference_text.setLocation(0,180);
-		preference_text.setFont(new Font("Arial",Font.BOLD,12));
-		preference_text.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(preference_text);
-		
-		JCheckBox dulce = new JCheckBox();
-		dulce.setSize(80,20);
-		dulce.setLocation(40,240);
-		dulce.setBackground(Color.white);
-		dulce.setFont(new Font("Arial",Font.PLAIN,13));
-		dulce.setText("dulce");
-		register_container.add(dulce);
-		
-		JCheckBox salado = new JCheckBox();
-		salado.setSize(80,20);
-		salado.setLocation(120,240);
-		salado.setBackground(Color.white);
-		salado.setFont(new Font("Arial",Font.PLAIN,13));
-		salado.setText("Salado");
-		register_container.add(salado);
-		
-		JCheckBox saludable = new JCheckBox();
-		saludable.setSize(90,20);
-		saludable.setLocation(200,240);
-		saludable.setBackground(Color.white);
-		saludable.setFont(new Font("Arial",Font.PLAIN,13));
-		saludable.setText("Saludable");
-		register_container.add(saludable);
-		
-		JLabel terms = new JLabel();
-		terms.setText("Preferencias");
-		terms.setSize(320,105);
-		terms.setLocation(0,180);
-		terms.setFont(new Font("Arial",Font.BOLD,12));
-		terms.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(terms);
-		//Terminos
-		JLabel condiciones = new JLabel();
-		condiciones.setText("Política de privacidad");
-		condiciones.setSize(240,25);
-		condiciones.setLocation(40,265);
-		condiciones.setFont(new Font("Arial",Font.PLAIN,15));
-		condiciones.setForeground(new Color(66,133,244));
-		condiciones.setHorizontalAlignment(JLabel.CENTER);
-		register_container.add(condiciones);
-		
-		
-		JRadioButton reject_terms = new JRadioButton("Rechazar");
-		reject_terms.setBounds(200,282,150,25);
-		reject_terms.setOpaque(false);
-		register_container.add(reject_terms);
-		
-		JRadioButton accept_terms = new JRadioButton("Acceptar");
-		accept_terms.setBounds(40,282,150,25);
-		accept_terms.setOpaque(false);
-		register_container.add(accept_terms);
-		
-		// Grupo excluyente
-		ButtonGroup group_terms = new ButtonGroup();
-		group_terms.add(accept_terms);
-		group_terms.add(reject_terms);
-		
-        String[] colonias = {
-                "Camino Real",
-                "Santa Fe",
-                "Miramar",
-                "Francisco Villa"
-            };
-		JComboBox<String> comboColonias = new JComboBox<>(colonias);
-	    comboColonias.setBounds(80, 315, 160, 30);
-	    register_container.add(comboColonias);
-	    
-		JButton new_account2 = new JButton();
-		new_account2.setSize(240,35);
-		new_account2.setLocation(40,350);
-		new_account2.setBackground(Color.white);
-		new_account2.setFont(new Font("Arial",Font.BOLD,14));
-		new_account2.setText("Crear cuenta");
-		new_account2.setBorder(BorderFactory.createLineBorder(new Color(200,200,200),1));
-		register_container.add(new_account2);
-		
+		// tarjeta principal de tabla
+		JPanel card_tabla = new JPanel();
+		card_tabla.setLayout(null);
+		card_tabla.setBackground(Color.white);
+		card_tabla.setBounds(50,40,900,500);
+		card_tabla.setBorder(BorderFactory.createLineBorder(new Color(220,220,220),1));
+		contenedor.add(card_tabla);
+
+		// titulo
+		JLabel titulo = new JLabel("USUARIOS");
+		titulo.setBounds(0,20,900,40);
+		titulo.setFont(new Font("Arial",Font.BOLD,26));
+		titulo.setHorizontalAlignment(JLabel.CENTER);
+		card_tabla.add(titulo);
+
+		// subtitulo
+		JLabel subtitulo = new JLabel("Registros de usuarios del sistema");
+		subtitulo.setBounds(0,55,900,25);
+		subtitulo.setFont(new Font("Arial",Font.PLAIN,14));
+		subtitulo.setForeground(new Color(120,120,120));
+		subtitulo.setHorizontalAlignment(JLabel.CENTER);
+		card_tabla.add(subtitulo);
+
+		// BOTONES
+		JButton exportar = new JButton("Exportar");
+		exportar.setBounds(650,100,110,35);
+		exportar.setBackground(Color.white);
+		exportar.setFont(new Font("Arial",Font.BOLD,14));
+		exportar.setBorder(BorderFactory.createLineBorder(new Color(200,200,200),1));
+		card_tabla.add(exportar);
+
+		JButton anadir = new JButton("Añadir");
+		anadir.setBounds(770,100,110,35);
+		anadir.setBackground(new Color(66,133,244));
+		anadir.setForeground(Color.white);
+		anadir.setFont(new Font("Arial",Font.BOLD,14));
+		anadir.setBorder(BorderFactory.createLineBorder(new Color(66,133,244),1));
+		card_tabla.add(anadir);
+
+		// columnas
+		String[] columnas = {
+			"ID",
+			"Nombre",
+			"Correo electrónico",
+			"Edad",
+			"Estado"
+		};
+
+		// datos
+		Object[][] datos = {
+				{"001","Carlos López","carlos@gmail.com","20","Activo"},
+				{"002","Ana Torres","ana@gmail.com","22","Activo"},
+				{"003","Luis Pérez","luis@gmail.com","19","Inactivo"},
+				{"004","María Gómez","maria@gmail.com","21","Activo"},
+				{"005","Jorge Ruiz","jorge@gmail.com","23","Inactivo"},
+				{"006","Sofía Díaz","sofia@gmail.com","20","Activo"},
+				{"007","Pedro Sánchez","pedro@gmail.com","24","Activo"},
+				{"008","Elena Castro","elena@gmail.com","22","Inactivo"},
+				{"009","Laura Méndez","laura@gmail.com","25","Activo"},
+				{"010","Diego Ramos","diego@gmail.com","18","Inactivo"},
+				{"011","Fernanda Ortiz","fernanda@gmail.com","21","Activo"},
+				{"012","Ricardo Vega","ricardo@gmail.com","26","Activo"},
+				{"013","Camila Navarro","camila@gmail.com","19","Inactivo"},
+				{"014","Andrés Morales","andres@gmail.com","23","Activo"},
+				{"015","Valeria Rojas","valeria@gmail.com","20","Activo"},
+				{"016","Miguel Herrera","miguel@gmail.com","27","Inactivo"},
+				{"017","Daniela Cruz","daniela@gmail.com","22","Activo"},
+				{"018","José Castillo","jose@gmail.com","24","Activo"},
+				{"019","Paula Vargas","paula@gmail.com","18","Inactivo"},
+				{"020","Alejandro Paredes","alejandro@gmail.com","25","Activo"},
+				{"021","Lucía Romero","lucia@gmail.com","21","Activo"},
+				{"022","Hugo Salazar","hugo@gmail.com","28","Inactivo"},
+				{"023","Natalia Flores","natalia@gmail.com","20","Activo"},
+				{"024","Sergio Medina","sergio@gmail.com","26","Activo"},
+				{"025","Patricia León","patricia@gmail.com","23","Inactivo"},
+				{"026","Gabriel Peña","gabriel@gmail.com","22","Activo"},
+				{"027","Mariana Silva","mariana@gmail.com","19","Activo"},
+				{"028","Iván Delgado","ivan@gmail.com","24","Inactivo"},
+				{"029","Carolina Campos","carolina@gmail.com","27","Activo"},
+				{"030","Emilio Fuentes","emilio@gmail.com","21","Activo"}
+			};
+		// MODELO (solo visual, no editable)
+		DefaultTableModel modelo = new DefaultTableModel(datos, columnas) {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
+		// TABLA
+		JTable tabla = new JTable(modelo);
+		tabla.setBounds(40,150,820,300);
+		tabla.setFont(new Font("Arial",Font.PLAIN,13));
+		tabla.setRowHeight(25);
+		tabla.setGridColor(new Color(230,230,230));
+		tabla.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS); // columnas redimensionables
+
+		// encabezado
+		tabla.getTableHeader().setFont(new Font("Arial",Font.BOLD,14));
+		tabla.getTableHeader().setBackground(new Color(245,245,245));
+		tabla.getTableHeader().setBorder(
+			BorderFactory.createLineBorder(new Color(220,220,220),1)
+		);
+
+		// scrollpane
+		JScrollPane scroll_tabla = new JScrollPane(tabla);
+		scroll_tabla.setBounds(40,150,820,300);
+		scroll_tabla.setBorder(
+			BorderFactory.createLineBorder(new Color(220,220,220),1)
+		);
+		card_tabla.add(scroll_tabla);
+
 		contenedor.repaint();
 		contenedor.revalidate();
+		this.setVisible(true);
 	}
 }
